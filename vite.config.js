2f0b5b0fb8d20db8624a8bfdee671ee556cb7f1f
Hamza Baloch <158695894+Hamzabaloch08@users.noreply.github.com>
2025-09-04 21:40:04 +0500

    .

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  base: "/", // Important for React Router in Vercel
-})
+  plugins: [react(), tailwindcss()],
+  base: "/", // For React Router in Vercel
+});
