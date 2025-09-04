b7a6fbada46ce2371fead7756b3102e99058d943
Hamza Baloch <158695894+Hamzabaloch08@users.noreply.github.com>
2025-09-04 22:08:41 +0500

    Update DashboardLayout.jsx

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-import Home from "../pages/home";
 import Navbar from "../components/common/Navbar";
+import Home from "../pages/home";
 
 const DashboardLayout = () => {
   return (
