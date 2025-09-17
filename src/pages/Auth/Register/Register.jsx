0d46183c98368259a3ed11e5caaf593f7973586d
Hamza Baloch <158695894+Hamzabaloch08@users.noreply.github.com>
2025-09-17 20:43:09 +0500

    Update Register.jsx

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -144,6 +144,7 @@ const Register = () => {
             ) : null}
             {registerStatus === "loading" ? "Registering..." : "Register"}
           </button>
+          {console.log(registerStatus)}
         </form>
 
         <p className="mt-6 text-center text-sm text-gray-600 dark:text-gray-400">
