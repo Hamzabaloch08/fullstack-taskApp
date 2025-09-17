5597f8462907fbd3a15b2398ed87eb0923523c1a
Hamza Baloch <158695894+Hamzabaloch08@users.noreply.github.com>
2025-09-17 23:34:49 +0500

    Update tasksApi.js

--- src/api/tasksApi.js
+++ src/api/tasksApi.js
@@ -1,13 +1,35 @@
 import axiosInstance from "./axiosInstance";
 
+const token = localStorage.getItem("token");
+
 // Fetch all tasks
-export const getTasksApi = () => axiosInstance.get("/tasks");
+export const getTasksApi = () =>
+  axiosInstance.get("/tasks", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
 // Add new task
-export const addTaskApi = (newTask) => axiosInstance.post("/tasks", newTask);
+export const addTaskApi = (newTask) =>
+  axiosInstance.post("/tasks", newTask, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
 // Delete task
-export const deleteTaskApi = (id) => axiosInstance.delete(`/tasks/${id}`);
+export const deleteTaskApi = (id) =>
+  axiosInstance.delete(`/tasks/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
 // Update task
-export const updateTaskApi = (id, data) => axiosInstance.put(`/tasks/${id}`, data);
+export const updateTaskApi = (id, data) =>
+  axiosInstance.put(`/tasks/${id}`, data, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
