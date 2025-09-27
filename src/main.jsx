d5bfddb0c04fda0f1ba1425d65ff021fcc021e6a
Hamza Baloch <158695894+Hamzabaloch08@users.noreply.github.com>
2025-09-27 16:41:23 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,10 @@ import "./index.css";
 import store from "./app/store.js";
 import { Provider } from "react-redux";
 
-
-
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App/>
+      <App />
     </Provider>
   </StrictMode>
 );
